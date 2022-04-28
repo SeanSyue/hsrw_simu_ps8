8f619c6f68333f52e97ad42c8106fc8bf46e6cff
SeanSyue <32350820+SeanSyue@users.noreply.github.com>
2022-04-28 10:36:37 +0200

    add sir_1.R

---
+++ sir_1.R
@@ -0,0 +1,40 @@
+## Load deSolve package
+pacman::p_load(deSolve)
+
+## Create an SIR function
+sir <- function(time, state, parameters) {
+  
+  with(as.list(c(state, parameters)), {
+    
+    dS <- -alpha * S * I
+    dI <-  alpha * S * I - beta * I
+    dR <-                 beta * I
+    
+    return(list(c(dS, dI, dR)))
+  })
+}
+
+### Set parameters
+## Proportion in each compartment: Susceptible 0.999999, Infected 0.000001, Recovered 0
+init       <- c(S = 1-1e-6, I = 1e-6, R = 0.0)
+## alpha: infection parameter; beta: recovery parameter
+parameters <- c(alpha = 1.5, beta = 0.15)
+## Time frame
+times      <- seq(0, 70, by = 1)
+
+## Solve using ode (General Solver for Ordinary Differential Equations)
+out <- ode(y = init, times = times, func = sir, parms = parameters)
+## change to data frame
+out <- as.data.frame(out)
+## Delete time variable
+out$time <- NULL
+## Show data
+head(out, 10)
+
+## Plot
+matplot(x = times, y = out, type = "l",
+        xlab = "Time", ylab = "Susceptible and Recovered", main = "SIR {alpha}=1.5, {beta}=0.15",
+        lwd = 1, lty = 1, bty = "l", col = 2:4)
+
+## Add legend
+legend(40, 0.7, c("Susceptible", "Infected", "Recovered"), pch = 1, col = 2:4, bty = "n")
